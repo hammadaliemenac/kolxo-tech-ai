2ed7aaa2abe6dd9fafde969c2070968dbc2fefa9
root <root@srv1484937.hstgr.cloud>
2026-03-13 04:10:40 +0000

    initial code

---
+++ app.py
@@ -0,0 +1,19 @@
+from ollama import chat
+
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello FastAPI on Ubuntu!"}
+
+@app.get("/ai-generator/")
+def read_item(query: str = 'Hello!'):
+    response = chat(
+        model='tinyllama',
+        messages=[{'role': 'user', 'content': query}],
+    )
+    return {"query": query, "content": response.message.content}
