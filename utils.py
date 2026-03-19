f08b1890fe6145343f067baf4f3b0fcd0a41e900
Hammad Ali <hali35275@gmail.com>
2026-03-19 14:23:22 +0500

    plagrism checker tool 1

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import language_tool_python
 from sentence_transformers import SentenceTransformer
 
-
-tool = language_tool_python.LanguageTool('en-US')
+try:
+    tool = language_tool_python.LanguageTool('en-US')
+except Exception as e:
+    print("LanguageTool initialization failed:", e)
+    tool = None
+    
 model = SentenceTransformer('all-MiniLM-L6-v2')
