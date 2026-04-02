eb67cec22bddb74a1d4df15217c85cc0616c89ce
Hammad Ali <hali35275@gmail.com>
2026-04-02 10:45:44 +0500

    update remove bg 2

--- routes/remove_bg.py
+++ routes/remove_bg.py
@@ -12,12 +12,12 @@ router = APIRouter()
 async def remove_bg(file: UploadFile = File(...)):
     input_bytes = await file.read()
 
-    input_image = Image.open(io.BytesIO(input_bytes)).convert("RGB")
+    input_image = Image.open(io.BytesIO(input_bytes)).convert("RGBA")
 
     output = remove(
         input_image,
         session=session,
-        alpha_matting=True,
+        alpha_matting=False,
         alpha_matting_foreground_threshold=240,
         alpha_matting_background_threshold=10,
         alpha_matting_erode_size=10
