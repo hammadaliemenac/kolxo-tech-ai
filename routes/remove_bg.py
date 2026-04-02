b358b18f685d5b7b822a7c6ef3ecf7c7b2af6986
Hammad Ali <hali35275@gmail.com>
2026-04-02 11:09:50 +0500

    update remove bg 5

--- routes/remove_bg.py
+++ routes/remove_bg.py
@@ -5,7 +5,6 @@ from PIL import Image
 import io
 
 session = new_session("isnet-general-use")
-# session = new_session("u2net_human_seg")
 router = APIRouter()
 
 @router.post("/remove-bg")
@@ -17,10 +16,7 @@ async def remove_bg(file: UploadFile = File(...)):
     output = remove(
         input_image,
         session=session,
-        alpha_matting=True,
-        alpha_matting_foreground_threshold=250,
-        alpha_matting_background_threshold=5,
-        alpha_matting_erode_size=10
+        alpha_matting=False
     )
 
     buf = io.BytesIO()
