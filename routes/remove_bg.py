e0c88699c3eeeba06b4619441ba582a595729f06
Hammad Ali <hali35275@gmail.com>
2026-04-02 11:41:35 +0500

    update remove bg 8

--- routes/remove_bg.py
+++ routes/remove_bg.py
@@ -5,7 +5,6 @@ from PIL import Image
 import io
 
 session = new_session("isnet-general-use")
-# session = new_session("u2net_human_seg")
 router = APIRouter()
 
 @router.post("/remove-bg")
@@ -17,11 +16,21 @@ async def remove_bg(file: UploadFile = File(...)):
     output = remove(
         input_image,
         session=session,
-        alpha_matting=False,
-        alpha_matting_foreground_threshold=240,
-        alpha_matting_background_threshold=10,
-        alpha_matting_erode_size=10
+        alpha_matting=False
     )
+
+    # 🔥 Fix fade issue
+    output = output.convert("RGBA")
+    pixels = output.load()
+
+    for y in range(output.size[1]):
+        for x in range(output.size[0]):
+            r, g, b, a = pixels[x, y]
+
+            if a < 180:
+                pixels[x, y] = (0, 0, 0, 0)
+            else:
+                pixels[x, y] = (r, g, b, 255)
 
     buf = io.BytesIO()
     output.save(buf, format="PNG")
